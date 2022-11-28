d700488d52dc40f5a06249435ba797d25a3fe94e
michael vivanco <mvivancovelez321@gmail.com>
2022-11-28 08:53:27 -0500

    rutas hasta antes de las secundarias

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { CatalogueComponent } from './pages/catalogue/catalogue.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
+import { ProductComponent } from './pages/product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //rutas protegidas
+  
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'product', component: ProductComponent},
+  {path: 'catalogue', component: CatalogueComponent},
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  {path: '**', component: NotfoundComponent},
+  
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
